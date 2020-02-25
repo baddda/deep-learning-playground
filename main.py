aa3691af9dda0fe6c91db3cc728462d23b331087
Alexej Hamburg <alexej@simpletechs.net>
2020-02-25 21:22:15 +0100

    add learning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import keras
 from keras.datasets import imdb
+from keras import models
+from keras import layers
+from keras import optimizers
 import numpy as np
 
  
@@ -24,6 +27,11 @@ def main():
     x_test = vectorize_sequeces(test_data)
     print(x_train[0])
 
+    # Array of booleans. Value of 1 means positive review.
+    y_train = np.asarray(train_labels).astype('float32')
+    y_test = np.asarray(test_labels).astype('float32')
+    print(y_train)
+
     # In this data set a text consists of an array of word indices, where each id is representing a word.
     word_index = imdb.get_word_index()
 
@@ -31,5 +39,24 @@ def main():
     reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 
     print(decode_word(train_data[4], reverse_word_index))
+    
+    # Set up nodes.
+    # Will look like this: 10000 nodes -> 16 nodes -> 16 nodes -> 1 nodes
+    # The activiation function is the function on how the combined input to a node should be passed to the next node.
+    model = models.Sequential()
+    model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    # Set up the optimizer, so basically the method of finding a global minimum in our neural network function.
+    # Set up the loss function, so the backpropagation knows the direction.
+    # Set up the metrics function. It's basically just to see how our model is performing. So it is not used for learning.
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+    # Train the network.
+    model.fit(x_train, y_train, epochs=4, batch_size=512)
+    results = model.evaluate(x_test, y_test)
+
 
 main()
